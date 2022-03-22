e92ebd58a3cfcee4e52aae18490233f82b373f08
Alberto Monteiro <alberto.monteiro@live.com>
2022-03-22 11:26:13 -0300

    Add project files.

---
+++ src/WomanInTechRabbitMQ.Consumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+var connectionFactory = new ConnectionFactory()
+{
+    Uri = new Uri("ampq://guest:guest@localhost")
+};
+
+using var connection = connectionFactory.CreateConnection();
+using var model = connection.CreateModel();
+
+model.ExchangeDeclare("woman-in-tech-exchange", ExchangeType.Direct);
+model.QueueDeclare("woman-in-tech-queue", true, false, false);
+model.QueueBind("woman-in-tech-queue", "woman-in-tech-exchange", "");
+
+var json = JsonSerializer.Serialize(new { Evento = "Woman in Tech - Banco Carrefour" });
+var bytes = Encoding.UTF8.GetBytes(json);
+
+model.BasicPublish("woman-in-tech-exchange", "", model.CreateBasicProperties(), bytes.AsMemory());
+
+Console.WriteLine("Mensagem enviada");
---
+++ src/WomanInTechRabbitMQ.Producer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text.Json;
+
+var connectionFactory = new ConnectionFactory()
+{
+    Uri = new Uri("ampq://guest:guest@localhost")
+};
+
+using var connection = connectionFactory.CreateConnection();
+using var model = connection.CreateModel();
+
+model.ExchangeDeclare("woman-in-tech-exchange", ExchangeType.Direct);
+model.QueueDeclare("woman-in-tech-queue", true, false, false);
+model.QueueBind("woman-in-tech-queue", "woman-in-tech-exchange", "");
+
+var consumer = new EventingBasicConsumer(model);
+consumer.Received += (sender, eventArgs) =>
+{
+    var body = eventArgs.Body;
+    var data = JsonSerializer.Deserialize<Message>(body.Span);
+
+    Console.WriteLine($"Recebido mensagem do evento: {data.Evento}");
+};
+
+model.BasicConsume("woman-in-tech-queue", false, consumer);
+
+Console.WriteLine("Mensagem enviada");
+
+public record Message(string Evento);
